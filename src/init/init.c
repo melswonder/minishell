93940e0141b1ffe6573b7363e3d56e34a5dfac64
Hiro Watanabe <hirwatan@c6r1s8.42tokyo.jp>
2025-04-11 13:51:55 +0900

    fix:initのノームの修正

--- src/init/init.c
+++ src/init/init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kiwasa <kiwasa@student.42.jp>              +#+  +:+       +#+        */
+/*   By: hirwatan <hirwatan@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 22:00:20 by kiwasa            #+#    #+#             */
-/*   Updated: 2025/04/10 22:45:25 by kiwasa           ###   ########.fr       */
+/*   Updated: 2025/04/11 13:50:27 by hirwatan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,38 +26,43 @@ t_shell	*init_shell(t_env *env)
 	return (shell);
 }
 
+t_env	*create_init_env_node(char *env_str)
+{
+	t_env	*new_node;
+	char	*equal_sign;
+
+	new_node = malloc(sizeof(t_env));
+	if (!new_node)
+		return (NULL);
+	equal_sign = strchr(env_str, '=');
+	if (equal_sign)
+	{
+		new_node->key = strndup(env_str, equal_sign - env_str);
+		new_node->value = strdup(equal_sign + 1);
+	}
+	else
+	{
+		new_node->key = strdup(env_str);
+		new_node->value = strdup("");
+	}
+	new_node->next = NULL;
+	return (new_node);
+}
+
 t_env	*init_env(char **envp)
 {
-	t_env	*head;
-	t_env	*tail;
-	t_env	*new_node;
-	int		i;
-	char	*equal_sign;
-	char	*key;
-	char	*value;
+	t_env		*head;
+	t_env		*tail;
+	t_env		*new_node;
+	static int	i = 0;
 
 	head = NULL;
 	tail = NULL;
-	i = 0;
 	while (envp[i])
 	{
-		new_node = malloc(sizeof(t_env));
+		new_node = create_init_env_node(envp[i]);
 		if (!new_node)
 			return (NULL);
-		equal_sign = strchr(envp[i], '=');
-		if (equal_sign)
-		{
-			key = strndup(envp[i], equal_sign - envp[i]);
-			value = strdup(equal_sign + 1);
-		}
-		else
-		{
-			key = strdup(envp[i]);
-			value = strdup("");
-		}
-		new_node->key = key;
-		new_node->value = value;
-		new_node->next = NULL;
 		if (!head)
 		{
 			head = new_node;
